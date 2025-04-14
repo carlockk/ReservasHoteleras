e7729065e9e240c7476dd2c980ce56ae28e3f111
carlockk <carlos.virtualdesk@gmail.com>
2025-04-14 02:21:17 -0400

    reservas.routes.js
    Se agregaron comentarios swagger

--- reservasHotelRender/src/routes/reservas.routes.js
+++ reservasHotelRender/src/routes/reservas.routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/reservas.controller');
-
-router.post('/', controller.createReserva);
+/**
+ * @swagger
+ * /reservas:
+ *   get:
+ *     summary: Obtener todas las reservas
+ *     tags: [Reservas]
+ *     responses:
+ *       200:
+ *         description: Lista de reservas
+ */
 router.get('/', controller.getAllReservas);
-router.get('/:id', controller.getReservaById);
-router.put('/:id', controller.updateReserva);
-router.delete('/:id', controller.deleteReserva);
-
-module.exports = router;
